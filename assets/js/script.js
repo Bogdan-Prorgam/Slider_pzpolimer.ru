6ad41573f86644db82da5044b46d15768112123a
Bogdan-Program <musixin.bogdan@mail.ru>
2022-09-30 13:33:59 +0500

    fix mobile img slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,6 @@ var swiper = new Swiper('.swiper', {
 });
 
 if(window.innerWidth < 1000){
-	document.querySelector("#slider_image_1").src="./assets/img/slider/slider_img_modile_1.png";
-	document.querySelector("#slider_image_2").src="./assets/img/slider/slider_img_modile_2.png";
-	document.querySelector("#slider_image_3").src="./assets/img/slider/slider_img_modile_3.png";
 	document.querySelectorAll(".swiper-slide")[0].style.backgroundImage = "linear-gradient(rgba(1, 22, 59, 0.6), rgba(1, 22, 59, 0.6)), url('./assets/img/slider/img_bg_1.png')";
 	document.querySelectorAll(".swiper-slide")[1].style.backgroundImage = "linear-gradient(rgba(7, 45, 90, 0.6), rgba(7, 45, 90, 0.6)), url('./assets/img/slider/img_bg_2.png')";
 	document.querySelectorAll(".swiper-slide")[2].style.backgroundImage = "linear-gradient(rgba(196, 7, 7, 0.6), rgba(196, 7, 7, 0.6)), url('./assets/img/slider/img_bg_3.png')";
